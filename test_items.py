45f504281b2c9ea1da7c5d3e8128e58930b9cd7a
Iuliia Sharina <yulia.hydrologist@yandex.ru>
2021-04-08 15:17:37 +0300

    Изменен файл conftest.py

--- test_items.py
+++ test_items.py
@@ -1,7 +1,9 @@
+#import time
 
-def test_add_to_cart_button(browser):
+def test_is_there_add_to_basket_button(browser):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    browser.find_element_by_css_selector("button.btn.btn-lg.btn-primary.btn-add-to-basket")
+    #time.sleep(30)
+        
+    assert browser.find_element_by_css_selector(".btn-add-to-basket"), "button is not exist"
            
-
